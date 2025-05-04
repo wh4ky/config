96d62e19074f9e23c80843545446adbd16cdeb0e
whacky <damian.whacky@proton.me>
2025-05-04 12:45:39 +0200

    Update
    Added to the neovim configuration.
    Fixed hyprland file browser not launching.
    A

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,9 +2,9 @@
 --  NOTE: Leader is set as space.
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.opt.termguicolors = true
+vim.g.have_nerd_font = false -- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.o.relativenumber = true
 
 -- [[ Options ]]
 require("options")
