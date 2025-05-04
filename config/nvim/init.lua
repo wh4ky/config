8c8dc004c5fd1b0f9eb45cd8ef38a8375280926d
whacky <damian.whacky@proton.me>
2025-05-04 12:50:22 +0200

    Fix
    Removed Hyprland second monitor config.
    Added Nerd font support in NeoVim.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,7 +3,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.termguicolors = true
-vim.g.have_nerd_font = false -- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.o.relativenumber = true
 
 -- [[ Options ]]
