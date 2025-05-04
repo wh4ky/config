86a29b0012605a33b8ba73cae3a90cce7646ef1a
Damian Czapnik <damian.whacky@proton.me>
2025-05-04 10:16:31 +0200

    Minor update.

--- install.sh
+++ install.sh
@@ -1,51 +1,88 @@
 #!/usr/bin/env bash
 
+# The following code is derived from Oh My Zsh
+# https://github.com/ohmyzsh/ohmyzsh
+# Licensed under the MIT License.
+
+# Start of borrowed code
+
+# Make sure important variables exist if not already defined
+#
+# $USER is defined by login(1) which is not always executed (e.g. containers)
+# POSIX: https://pubs.opengroup.org/onlinepubs/009695299/utilities/id.html
+USER=${USER:-$(id -u -n)}
+# $HOME is defined at the time of login, but it could be unset. If it is unset,
+# a tilde by itself (~) will not be expanded to the current user's home directory.
+# POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
+HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
+# macOS does not have getent, but this works even if $HOME is unset
+HOME="${HOME:-$(eval echo ~$USER)}"
+
+# End of borrowed code
+
 OS=$(uname)
 ARCH=$(uname -m)
-CONFIGDIR=""
+# Set config directory
+CONFIGDIR=$HOME/.config
+# Set OhMyZsh install directory
+ZSH=$CONFIGDIR/ohmyzsh # If you change this, also change the ZSH variable in ./config/zsh/zshrc
 
-if [ "$XDG_CONFIG_HOME" = "" ]; then
-  echo "XDG_CONFIG_HOME not found, using $HOME/.config/"
-  CONFIGDIR=$HOME/.config/
-else
-  CONFIGDIR=$XDG_CONFIG_HOME
-fi
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
 
-echo "Detected OS: $OS $ARCH"
+printhelp() {
+  cat <<EOF
+Available arguments:
+  -h    This help dialog.
+  -r    Setup for remote (aimed at old Ubuntu-based servers or GitHub codespaces).
+EOF
+}
 
-# Set up for different distros
-case $OS in
-"Darwin")
-  cp -vr ./config/nvim $CONFIGDIR/nvim
-  cp -v ./config/zsh/zshrc $HOME/.zshrc
-  if [ $(uname -m) = "arm64" ]; then
-    bash scripts/darwin.sh
-  else
-    echo "Only Apple Silicon is supported."
-  fi
-  ;;
+main() {
+  while getopts "rh" opt; do
+    case $opt in
+    r) bash ./scripts/codespace.sh ;;
+    \?)
+      printhelp
+      exit 1
+      ;;
+    :)
+      printhelp
+      exit 1
+      ;;
+    esac
+  done
 
-"Linux")
-  LINUX=$(grep '^NAME=' /etc/os-release | cut -d '=' -f 2)
-  LINUX=${LINUX//\"/}
-  echo "Detected distribution: $LINUX"
+  echo "Detected OS: $OS $ARCH"
 
-  cp -vr ./config/foot $CONFIGDIR/foot
-  cp -vr ./config/hypr $CONFIGDIR/hypr
-  cp -vr ./config/nvim $CONFIGDIR/nvim
-  cp -v ./config/zsh/zshrc $HOME/.zshrc
+  exit 0
+  # Set up for different distros
+  case $OS in
+  "Darwin")
+    cp -vr ./config/nvim $CONFIGDIR/nvim
+    cp -v ./config/zsh/zshrc $HOME/.zshrc
+    if [ $ARCH = "arm64" ]; then
+      bash scripts/darwin.sh
+    else
+      echo "Only Apple Silicon is supported."
+    fi
+    ;;
 
-  case $LINUX in
-  "Ubuntu")
-    bash scripts/codespace.sh
+  "Linux")
+    LINUX=$(grep '^NAME=' /etc/os-release | cut -d '=' -f 2)
+    LINUX=${LINUX//\"/}
+    echo "Detected distribution: $LINUX"
+
+    cp -vr ./config/foot $CONFIGDIR/foot
+    cp -vr ./config/hypr $CONFIGDIR/hypr
+    cp -vr ./config/nvim $CONFIGDIR/nvim
+    cp -v ./config/zsh/zshrc $HOME/.zshrc
+    ;;
+  *)
+    echo "Unknown os!"
     ;;
   esac
-  ;;
-
-*)
-  echo "unknown os, exiting"
-  exit 0
-  ;;
-esac
-
+}
+main "$@"
 exit 0
--- scripts/darwin.sh
+++ scripts/darwin.sh
@@ -8,4 +8,12 @@ echo "Installing brew"
 # Put brew packages in $PATH
 echo >>$HOME/.zshrc
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>$HOME/.zshrc
+
+# BSD sed, add iterm2 plugin and terminal integration.
+sed -i '' '/^source \$ZSH\/oh-my-zsh\.sh/i\
+plugins+=(iterm2)\
+zstyle :omz:plugins:iterm2 shell-integration yes\
+\
+' $HOME/.zshrc
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
